5b9bcd8eb0dd36e3d846b9b0a5d9d8e66937d80d
Sachinmandial242 <rishumandial242@gmail.com>
2026-03-31 15:44:24 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSy...",
+    authDomain: "student-result-system-df097.firebaseapp.com",
+    projectId: "student-result-system-df097",
+    storageBucket: "student-result-system-df097.firebasestorage.app",
+    messagingSenderId: "917085884960",
+    appId: "1:917085884960:web:233e67f50a6811fa142245",
+    measurementId: "G-HQFJS752H3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
